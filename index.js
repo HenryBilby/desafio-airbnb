8ad3dfb86bd6a259b2b96c281de78542c1304319
Henry Miller Maia Bilby <hmmb-eng@outlook.com>
2020-05-08 21:16:07 -0400

    Validando os formularios de entrada de dados

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const cardsContainer = document.querySelector("#cards");
 var data = []
+var countDays = 0;
+
 
 function renderCard(card) {
     const div = document.createElement("div");
@@ -15,6 +17,9 @@ function renderCard(card) {
             <p class="card-text">
                 Preço: R$ ${card.price},00
             </p>
+            <p class="card-text">
+            Total: R$ ${card.price*countDays},00
+        </p>
         </div>
     </div>`;
     cardsContainer.appendChild(div);
@@ -31,9 +36,55 @@ async function fetchCards() {
     return await response.json();
 }
 
-async function handleClickSearch() {
+async function challengeAirbnb() {
     data = await fetchCards();
     if(data) {
         renderCards(data);
     }
 }
+
+function daysBetween(firstDate, lastDate) {
+    const oneDay = 24 * 60 * 60 * 1000;
+    return parseInt((lastDate - firstDate) / oneDay);
+}
+
+function validateForm() {
+    let form = document.getElementById("form-search");
+    form.classList.add("was-validated");
+
+    const invalidGroup = form.querySelectorAll(":invalid");
+
+    if (invalidGroup.length) {
+      return false;
+    }
+
+    let checkin = document.getElementById("checkin");
+    let checkout = document.getElementById("checkout");
+
+    let actualDate = new Date();
+    let checkinDate = new Date(checkin.value);
+    let checkoutDate = new Date(checkout.value);
+
+    if (checkoutDate <= checkinDate) {
+      let invalidFeedback = document.querySelectorAll(".date-fields");
+      checkout.classList.add("is-invalid");
+
+      invalidFeedback.forEach((div) => {
+        return (div.textContent = "Data de Checkout não pode ser menor ou igual a Data de Checkin");
+      });
+      return false;
+    }
+
+    countDays = daysBetween(checkinDate, checkoutDate);
+
+
+    return true;
+  }
+
+  function handleClickSearch() {
+    if (validateForm()) {
+        let clean_error_msg = document.querySelectorAll(".date-fields");
+        clean_error_msg.textContent = "";
+        challengeAirbnb();
+    }
+  }
