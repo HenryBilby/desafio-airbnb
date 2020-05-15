1e2a2f816d01a9ae40db1dc85d44080a4cbfc26a
Henry Miller Maia Bilby <hmmb-eng@outlook.com>
2020-05-14 20:34:47 -0400

    Fix script position

--- index.js
+++ index.js
@@ -20,28 +20,6 @@ function renderCard(card) {
             <p class="card-text">
                 Total: R$ ${card.price*countDays},00
             </p>
-            // <button type="button" class="button-submit" data-toggle="modal" data-target="#exampleModal">
-            //     Mapa
-            // </button>
-            // <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            //     <div class="modal-dialog" role="document">
-            //     <div class="modal-content">
-            //         <div class="modal-header">
-            //         <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-            //         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-            //             <span aria-hidden="true">&times;</span>
-            //         </button>
-            //         </div>
-            //         <div class="modal-body">
-            //         ...
-            //         </div>
-            //         <div class="modal-footer">
-            //         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-            //         <button type="button" class="btn btn-primary">Save changes</button>
-            //         </div>
-            //     </div>
-            //     </div>
-            // </div>
         </div>
     </div>`;
     cardsContainer.appendChild(div);
